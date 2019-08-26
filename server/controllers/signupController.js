50265463bf53f3c63b2bbf2694c92501a6cf050f
Mutesi Sharon K <sharonuase@gmail.com>
2019-08-26 16:29:03 +0200

    ft(signup endpoint)
     - added validification

--- server/controllers/signupController.js
+++ server/controllers/signupController.js
@@ -12,7 +12,57 @@ class userSignup {
             bio: req.body.bio,
             occuoation: req.body.occuoation,
             expertise: req.body.email
+
         }
+        if(!req.body.firstName){
+            res.status(400).send({
+                success: 'false',
+                message: 'first name is empty'
+            })
+
+        } else if(!req.body.lastName){
+            res.status(400).send({
+                success: 'false',
+                message: 'lastName is empty'
+            })
+
+        } else if(!req.body.email){
+            res.status(400).send({
+                success: 'false',
+                message: ' email is empty'
+            })
+
+        } else if(!req.body.password){
+            res.status(400).send({
+                success: 'false',
+                message: 'Password is empty'
+            })
+
+        } else if(!req.body.address){
+            res.status(400).send({
+                success: 'false',
+                message: 'Address is empty'
+            })
+
+        } else if(!req.body.bio){
+            res.status(400).send({
+                success: 'false',
+                message: 'bio is empty'
+            })
+
+        } else if(!req.body.occupation){
+            res.status(400).send({
+                success: 'false',
+                message: 'occupation is empty'
+            })
+
+        } else if(!req.body.expertise){
+            res.status(400).send({
+                success: 'false',
+                message: 'Expertise is empty'
+            })
+
+        } 
 
         return res.status(200).send({
             success: 'true',
