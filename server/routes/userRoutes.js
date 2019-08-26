6c92890d8f87a6f68ebb7ed5938e6b86b45faeda
Mutesi Sharon K <sharonuase@gmail.com>
2019-08-27 01:00:47 +0200

    ft(viewAllMentors endpoint): Create a view all mentors endpoint
    - added a view all mentors endpoint

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,13 @@
 import express from 'express';
-import usersignup from '../controllers/signupController'
+import usersignup from '../controllers/signupController';
+import userlogin from '../controllers/loginController';
+import viewmentors from '../controllers/viewAllMentorsController';
+
 const router = express.Router();
 
 router.post('/api/v1/signup', usersignup.createUser);
+router.post('/api/v1/login', userlogin.loginAccount);
+router.get('/api/v1/allMentors', viewmentors.getAllMentors)
+
 
 export default router;
